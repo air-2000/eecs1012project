b5e732f2c0b67cca4c398ed1b1c1477ce07900aa
morgangee123 <mattpallik@gmail.com>
2024-03-29 17:41:43 -0400

    Added Hamburger

---
+++ genres.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var menuIcon = document.querySelector('.menu-icon');
+    var menuOptions = document.querySelector('.menu-options');
+
+    menuIcon.addEventListener('click', function() {
+        menuOptions.classList.toggle('active');
+    });
+});
