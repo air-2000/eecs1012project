fa27e204d182eecfa63aa44d5b1f449da49aefa7
air-2000 <41229917+air-2000@users.noreply.github.com>
2024-04-04 19:16:18 -0400

    Minor css changes required to make it move
    hopefully

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
+
+var slides = document.querySelectorAll(".posters img");
+var activeSlide = slides[1];
+let SlideIndex = 1;
+
 document.addEventListener('DOMContentLoaded', function() {
     var menuIcon = document.querySelector('.menu-icon');
     var menuOptions = document.querySelector('.menu-options');
+
+    slides = document.querySelectorAll(".posters img");
+    activeSlide = slides[1];
 
     menuIcon.addEventListener('click', function() {
         menuOptions.classList.toggle('active');
     });
 });
+
+
+
+function showSlide(index){
+    if(index >= slides.length){
+        SlideIndex = 0;
+    }
+
+    else if(index < 0){
+        SlideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("activeposter");
+    });
+
+    slides[SlideIndex].classList.add("activeposter");
+}
+
+function rightbutton() {
+    SlideIndex++;
+    showSlide(SlideIndex);
+    console.log(SlideIndex);
+}
+
+function leftbutton() {
+    SlideIndex--;
+    showSlide(SlideIndex);
+    console.log(SlideIndex);
+}
